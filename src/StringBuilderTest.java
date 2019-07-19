739b8fdaa4c4170003308cc655ff2f6d05605d6b
Kriss <geraltrivia19@gmail.com>
2019-07-19 12:37:32 +0200

    Done.

--- src/StringBuilderTest.java
+++ src/StringBuilderTest.java
@@ -5,7 +5,7 @@ public class StringBuilderTest {
     private Scanner inputData = new Scanner(System.in);
     private int numberTest;
     private String stringVariable = "";
-    private StringBuilderTest builderVariable;
+    private StringBuilder builderVariable = new StringBuilder();
 
     public int getNumberTest() {
         return numberTest;
@@ -16,17 +16,44 @@ public class StringBuilderTest {
         numberTest = inputData.nextInt();
     }
 
-
     void testString() {
-        long a = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         for (int i = 0; i < numberTest; i++) {
+            stringVariable+="a";
         }
+
+        long end = (System.currentTimeMillis() - start)/1000;
+        String name = new Object(){}.getClass().getEnclosingMethod().getName();
+        testTime(name,numberTest,end);
+    }
+
+    void testStringBuilder(){
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numberTest; i++) {
+            builderVariable.append("a");
+        }
+
+        long end = (System.currentTimeMillis() - start);
+        String name = new Object(){}.getClass().getEnclosingMethod().getName();
+        testTime(name,numberTest,end);
+        testString();
+    }
+
+    void testTime(String name,int numberTest,long time){
+
+        System.out.printf("W funkcji %s, przy %d dodanych znaków, czas wykonania operacji był równy %d ms. \n",name,numberTest,time);
+    }
+
+    void conclusions(){
+
+        // W zależności w jakiej koejności zostają puszczone testy. testy należy wykonywać osobno.
     }
 
     public static void main(String[] args) {
         StringBuilderTest stringBuilderTest = new StringBuilderTest();
         stringBuilderTest.NumberOfTests();
-        System.out.println(stringBuilderTest.getNumberTest());
+//        System.out.println(stringBuilderTest.getNumberTest());
+        stringBuilderTest.testStringBuilder();
     }
 
 }
