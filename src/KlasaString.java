56e1baa3b6d752e44ce95d789e3e7f37a8b13347
Kriss <geraltrivia19@gmail.com>
2019-07-19 11:36:15 +0200

    Test klasy String

---
+++ src/KlasaString.java
@@ -0,0 +1,24 @@
+public class KlasaString {
+
+    private String name = "Krzysztof";
+    private String aName = new String();
+
+    void setaName(){
+        System.out.println(aName.length());
+        System.out.println(aName==null);
+        this.aName = "Krzysztof";
+        firstTest();
+    }
+
+    void firstTest(){
+        System.out.println(name.equals(aName));
+    }
+
+    public static void main(String[] args){
+        KlasaString aklasaString = new KlasaString();
+        aklasaString.setaName();
+
+
+
+    }
+}
